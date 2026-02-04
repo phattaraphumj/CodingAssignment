3c573d4d84775c32e1f9ae194fb9cda243084ffa
phattaraphumj <phattaraphumj@gmail.com>
2026-02-04 23:47:15 +0700

    add q3.cpp+timeComplexity

---
+++ q3.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    ll n;
+    
+    cin >> n;
+
+    ll i = 60; 
+
+    while (true) { 
+        ll mid = 1LL << (i - 1);
+
+        if (n == mid) {
+            char result = 'A' + ((i - 1) % 26);
+            cout << result << endl;
+            break;
+        } else if (n > mid) {
+            n -= mid;
+        }
+        i--;
+    }
+    //Time Complexity: O(log n) 
+    //เพราะloopแต่ละรอบ n ลดลงทีละครึ่ง(ในกรณีที่อยู่ขวาสุด)
+}
